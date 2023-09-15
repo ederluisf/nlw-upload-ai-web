12fa65c3f7239d5eb32c4f6d1b039d2d1350e2df
Sgt  EDER <ederelfs@SGT-EDER.ccasj.intraer>
2023-09-15 11:13:42 -0300

    Botão Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react';
 
 export function App() {
   return (
-    <Button>Teste</Button>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido no NLW da Rocketseat</span>
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2"></Github>            
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
