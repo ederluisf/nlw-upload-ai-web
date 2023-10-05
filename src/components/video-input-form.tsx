872b522b93578ad959650d673f87053bbb0b8164
Eder Fonseca <ederelfs@gmail.com>
2023-10-05 15:46:18 -0300

    Conversão de Vídeo para Áudio no cliente

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,10 +3,13 @@ import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Separator } from "./ui/separator";
 import { Textarea } from "./ui/textarea";
-import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null);
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
  
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget;
@@ -20,10 +23,57 @@ export function VideoInputForm() {
     setVideoFile(selectedFile);
   }
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+  async function convertVideoToAudio(video: File) {
+    console.log('Convert started.');
+    
+    const ffmpeg = await getFFmpeg();  
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video));
+
+    ffmpeg.on('log', log =>{
+      console.log(log);
+    });
+
+    ffmpeg.on('progress', progress =>{
+      console.log('Convert progress: ' + Math.round(progress.progress * 100));
+    })
+    
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3');
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' });
+    const audioFile = new File([audioFileBlob], 'audio.mp3', { type: 'audio/mpeg' });
+
+    console.log('Convert finished.');    
+
+    return audioFile;
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) {
+      return;
+    }
+
+    // Converter o vídeo em áudio - Convertendo no front para não sobrecarregar o back
+    const audioFile = await convertVideoToAudio(videoFile);
+
+    console.log(prompt);
+    console.log(audioFile);
   }
 
   const previewURL = useMemo(() => {
@@ -56,7 +106,8 @@ export function VideoInputForm() {
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
         <Textarea 
-          id="transcription_prompt" 
+          id="transcription_prompt"
+          ref={promptInputRef}
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)">
         </Textarea>
